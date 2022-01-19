e6695f457280625e6fc110646f6c672341c93a5d
David Wang <david2000wang@gmail.com>
2022-01-19 18:52:24 -0500

    imported flask, runs on local server

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 # list of coins and current amount
 import positions as p
 from py_coingecko import py_coingecko
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'hello world!'
+
+
+if __name__ == '__main__':
+    app.run()
 
 coingecko = py_coingecko()
 
@@ -26,12 +38,14 @@ total = 0
 for i in category_totals:
     total += i
 
-print(tickers)
-print(prices)
-print(total)
+# print(tickers)
+# print(prices)
+print(f'total: ${total:.2f}')
 
-print(f'short_term: {category_totals[0]:.2f} percentage: {category_totals[0]/total:.2%}')
-print(f'long_term: {category_totals[1]:.2f} percentage: {category_totals[1]/total:.2%}')
-print(f'day_trading: {category_totals[2]:.2f} percentage: {category_totals[2]/total:.2%}')
-print(f'stables: {category_totals[3]:.2f} percentage: {category_totals[3]/total:.2%}')
+print(f'short_term: ${category_totals[0]:.2f} percentage: {category_totals[0]/total:.2%}')
+print(f'long_term: ${category_totals[1]:.2f} percentage: {category_totals[1]/total:.2%}')
+print(f'day_trading: ${category_totals[2]:.2f} percentage: {category_totals[2]/total:.2%}')
+print(f'options: ${category_totals[3]:.2f} percentage: {category_totals[3]/total:.2%}')
+print(f'stables: ${category_totals[4]:.2f} percentage: {category_totals[4]/total:.2%}')
+
 
